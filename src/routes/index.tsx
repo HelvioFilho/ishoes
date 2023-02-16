35713b41540818d07ef8c8507bedf9571beb80e9
Hélvio Filho <helviosvf@gmail.com>
2023-02-16 14:12:48 -0300

    Feat: deep link configured in the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,20 @@ import OneSignal, { NotificationReceivedEvent, OSNotification } from "react-nati
 import { Notification } from "@components/Notification";
 import { AppRoutes } from "./app.routes";
 
+const linking = {
+  prefixes: ['com.hsvf.ishoes://', 'iShoes://'],
+  config: {
+    screens: {
+      details: {
+        path: 'details/:productId',
+        parse: {
+          productId: (productId: string) => productId
+        }
+      }
+    }
+  }
+}
+
 export function Routes() {
   const [notification, setNotification] = useState<OSNotification>();
   
@@ -24,7 +38,7 @@ export function Routes() {
   }, []);
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
       {
         notification?.title &&
