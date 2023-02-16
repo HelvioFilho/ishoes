fd400d6886890ae058aece6f0d22425e34d682e0
Hélvio Filho <helviosvf@gmail.com>
2023-02-16 14:05:37 -0300

    Feat: created push notification with onesignal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { Platform, StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import OneSignal from "react-native-onesignal";
 
-import { Routes } from "@routes/index";
 import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
 import { theme } from "./src/theme";
 
 import { CartContextProvider } from "@contexts/CartContext";
-import OneSignal from "react-native-onesignal";
+import { hasNotificationOpened, tagUserInfoCreate } from "@utils/notifications";
+import { useEffect } from "react";
 
 const { SIGNAL_ANDROID_ID } = process.env;
 const { SIGNAL_IOS_ID } = process.env;
@@ -15,12 +17,21 @@ const { SIGNAL_IOS_ID } = process.env;
 const oneSignalId = Platform.OS === "ios" ? SIGNAL_IOS_ID : SIGNAL_ANDROID_ID;
 
 OneSignal.setAppId(oneSignalId!);
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response);
-});
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
+  tagUserInfoCreate({
+    user_name: 'Hélvio',
+    user_email: 'helviosvf@gmail.com'
+  });
+
+  useEffect(() => {
+    const unsubscribe = hasNotificationOpened();
+
+    return () => unsubscribe;
+  }, []);
 
   return (
     <NativeBaseProvider theme={theme}>
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,45 +1,68 @@
-import { CloseIcon, HStack, Icon, IconButton, Text } from 'native-base';
+import { CloseIcon, HStack, Icon, IconButton, Text, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { OSNotification } from 'react-native-onesignal';
 
 type NotificationProps = {
-  title: string;
+  data: OSNotification;
   onClose: () => void;
 }
 
-export function Notification({title, onClose}: NotificationProps){
+type AdditionalDataProps = {
+  route?: 'details';
+  productId?: string;
+}
+
+export function Notification({ data, onClose }: NotificationProps) {
+
+  const { navigate } = useNavigation()
+
+  function handleOnPress() {
+    const { route, productId } = data.additionalData as AdditionalDataProps;
+    
+    if(route === 'details' && productId){
+      navigate(route, { productId });
+      onClose();
+    }
+  }
+
   return (
-    <HStack 
+    <Pressable
       p={4}
       pt={12}
-      w='full'
       top={0}
+      w='full'
       bgColor='gray.200'
       position='absolute'
-      alignItems='center'
-      justifyContent='space-between'
+      onPress={handleOnPress}
     >
-      <Icon 
-        as={Ionicons}
-        name='notifications-outline'
-        color='black'
-        size={5}
-        mr={2}
-      />
-      <Text
-        flex={1}
-        fontSize='md'
-        color='black'
+      <HStack
+        alignItems='center'
+        justifyContent='space-between'
       >
-        {title}
-      </Text>
-      <IconButton 
-        variant='unstyled'
-        _focus={{ borderWidth: 0 }}
-        icon={<CloseIcon size='3' />}
-        _icon={{ color: 'coolGray.600' }}
-        color='black'
-        onPress={onClose}
-      />
-    </HStack>
+        <Icon
+          as={Ionicons}
+          name='notifications-outline'
+          color='black'
+          size={5}
+          mr={2}
+        />
+        <Text
+          flex={1}
+          fontSize='md'
+          color='black'
+        >
+          {data.title}
+        </Text>
+        <IconButton
+          variant='unstyled'
+          _focus={{ borderWidth: 0 }}
+          icon={<CloseIcon size='3' />}
+          _icon={{ color: 'coolGray.600' }}
+          color='black'
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
   );
 }
